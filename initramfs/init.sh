9dbd74d301c93cd6a63b83765373e0ac0011d220
Bill Zissimopoulos <billziss@navimatics.com>
2023-04-17 23:01:23 +0100

    troubleshoot

--- initramfs/init.sh
+++ initramfs/init.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-mount -t devtmpfs none /dev
-mount -t proc none /proc
-mount -t sysfs none /sys
-/bin/busybox echo WORKSWORKSWORKSWORKSWORKSWORKSWORKSWORKS
+# mount -t devtmpfs none /dev
+# mount -t proc none /proc
+# mount -t sysfs none /sys
+/bin/busybox --install -s
+/bin/echo WORKSWORKSWORKSWORKSWORKSWORKSWORKSWORKS > /dev/console
+/bin/echo PATH=$PATH > /dev/console
 exec /bin/sh
