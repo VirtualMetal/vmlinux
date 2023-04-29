c5b8496a7e86f7c5802dd693fb5d1e0077570dd1
Bill Zissimopoulos <billziss@navimatics.com>
2023-04-29 13:14:14 +0100

    troubleshoot

--- initramfs/init.sh
+++ initramfs/init.sh
@@ -1,8 +1,6 @@
 #!/bin/busybox sh
 /bin/busybox --install
-/bin/echo 1111111 1111111 1111111 1111111
 /bin/mount -t devtmpfs none /dev
 /bin/mount -t proc none /proc
 /bin/mount -t sysfs none /sys
-/bin/echo 2222222 2222222 2222222 2222222
 exec /bin/sh
